bc9087c41fde334d08869f371170b48dfcbd25ac
FERNANDO JOSE VASQUEZ PACHANO <fevasque@ext.uc3m.es>
2025-10-07 16:07:25 +0200

    practicas realizadas el martes

---
+++ ciclos_for.py
@@ -0,0 +1,22 @@
+'''
+**** Practicas del ciclo for *****
+'''
+
+print('\nEJERCICIO 1.Imprimir los números del 1 al 10.' )
+
+for i in range(1,11):
+    print(i, end='-')
+
+print('\n\nEJERCICIO 2.Imprimir la tabla de multiplicar de cualquier numero .')
+
+n = int(input("Que tabla de multiplicar quieres ver?: "))
+for i in range(1,13):
+    resultado = n * i
+    print( n,"x",i,"=",resultado)
+
+
+print('\n\nEJERCICIO 3.Imprimir todos los números pares entre 1 y 50..')
+
+print('\n\nEJERCICIO 4.Calcular el factorial de un número dado' )
+
+print('\n\nEJERCICIO 5.Pedir 5 números al usuario y mostrar su promedio' )
